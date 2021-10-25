9f5e59ff49dc0a86f4bd2149b1fe1892736b80f9
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2021-10-25 00:11:39 -0700

    Callback vs promise statement

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const db = require('./config/keys');
+const mongoose = require('mongoose');
+
 
 // Write our first route
-app.get('/', (req, res) => res.send('Hello!'));
+app.get('/', (req, res) => res.send('Hello GAYWAD!'));
 
+// Connecting to database
+mongoose.connect(db.mongoURI)
+        .then(() => console.log('Mongodb connected'))
+        .catch((err) => console.log(err) );
+
+// Callback below. Promise statement above.
 const port = 5001;
 app.listen(port, () => console.log(`Server is running on port ${port}`) );
