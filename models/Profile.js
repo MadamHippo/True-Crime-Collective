31f20860d2a22fda626cbd34a89c0e86bb06a16a
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2021-11-06 20:11:21 -0700

    Profiles validation

--- models/Profile.js
+++ models/Profile.js
@@ -4,91 +4,95 @@ const Schema = mongoose.Schema; // within Mongoose, theres a library call Schema
 // like the user.js, you want to connect profile with user.
 // It will help id each user, if you put user id is 1 then it will help look up that relationship. Instead of copying. Always aim for short and small relationships so you don't waste performance. Have a simple relationship within mongodb.
 
+// instead of duplicating data, keep a simple relationship instead. In no sql, keep relationships flat.
+// every colum we create in users collection, we need to say what types of the schema is it.
 const ProfileSchema = new Schema({
+  //first column: it's the user column. It will tie into the users collection.
   user: {
     type: Schema.Types.ObjectId,
+    // the type of this column is then schema.types.objectId. The object that gets created is unique for each row so the user will refer to objectID in the collection below called 'users' [see ref]
     ref: 'users'
   },
   handle: {
+    // handle is like username...ie twitter handle
     type: String,
     required: true,
-    max: 40
+    max: 20
   },
-  company: {
+  age: {
+    type: Number,
+    required: true
+  },
+  occupation: {
     type: String
   },
-  website: {
+  education: {
     type: String
   },
   location: {
     type: String
   },
-  status: {
-    type: String,
-    required: true
-  },
-  skills: {
-    type: [String],
+  hobbies: {
+    type: [String], // you can have more than 1 element in the array
     required: true
   },
   bio: {
     type: String
   },
-  githubusername: {
-    type: String
-  },
-  experience: [{
-    title: {
-      type: String,
-      requried: true
+
+  // experience is an array of objects, so we can add as many entries.
+
+  fav_crime_cases: [{
+    perp: {
+      type: [String],
+      required: true
     },
-    company: {
-        type: String,
+    victim: {
+        type: [String],
         required: true
     },
-    location: {
+    about: {
       type: String
     },
     from: {
-      type: Date,
-      required: true
+      type: Date
     },
     to: {
       type: Date
     },
-    current: {
+    active_case: {
       type: Boolean,
       default: false
     },
-    description: {
-      type: String
+    theories: {
+     type: String
     }
   }],
-  education: [{
-    school: {
+
+  // education is an array, so we can add as many entries.
+
+  memorial: [{
+    victim: {
       type: String,
       required: true
     },
-    degree: {
-      type: String,
-      required: true
-    },
-    fieldofstudy: {
+    in_memory: {
       type: String,
       required: true
     },
     from: {
-      type: Date,
-      required: true
+      type: Date
     },
     to: {
       type: Date
     },
-    current: {
+    living: {
       type: Boolean,
       default: false
     }
   }],
+
+  // No social media is required but can be included in the model
    social: {
     youtube: {
       type: String
@@ -109,3 +113,6 @@ const ProfileSchema = new Schema({
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
+//asking mongoose to create a model, called profile model in the table. And that's what is being exported from the model.
+
+// Next step is focusing on the API. We are following the MVC pattern. Model is done. 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,334 @@
+// Bringing in express, router, passport just like in users.js
+// Do not need mongoose so I deleted it
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
+
+// we need both profile and user models since they're interdependent
+//the user will first come to your page...then register their account. Now they're in the database, then they will login. Now they have logged in (getting a token), now send the user to the "dashboard page" which is nothing but the user's profile page. (Path for that is API/profile in postman)
+
+// The client has a token and now they will send a GET API to get their profile. We need a GET API (below) which will query the profiles collection and ask if this user has this particiular profile yet. It will ask hey does this uer have a profile yet?!
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Model
+const User = require("../../models/User");
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 
-router.get('/test', (req, res) => res.json({msg: 'Profile works!'}));
+
+
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+// private because no one should be able to view or edit your profile
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  // calling passport because it's private API 
+  (req, res) => {
+    const errors = {};
+
+    // like User.findOne, we're asking profile to find ONE collection based on the User column
+    Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
+      .then((profile) => {
+        //(if no profile...done on the react side)
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then((profiles) => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch((err) => res.status(404).json(err));
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch((err) =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch((err) => res.status(404).json(err));
+});
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+
+    // Due to volume of things, we are creating an object called profileFields. It should match profile schema.
+    // Built by requesting user id to confirm id
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // Skills - Spilt the array of skills using a delimiter. If the skill isn't undefined, then we will split an array. Then it will be submitted to the database.
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    // Social media
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      if (profile) {
+        // Update...you need to find your data in the document and then update it with new information.
+        // Maybe you're adding new data, not just updating existing ones.
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then((profile) => res.json(profile));
+
+      // assuming user has no profile yet...go to else
+
+      } else {
+        // Create
+
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then((profile) => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            return res.status(400).json(errors);
+          }
+          // Save Profile - created a new schema of profile if theres no profile existing and saving completed profile to the database.
+          // No catch, everything works out
+          new Profile(profileFields)
+            .save()
+            .then((profile) => res.json(profile));
+        });
+      }
+    });
+  }
+);
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+      };
+
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then((profile) => res.json(profile));
+    });
+  }
+);
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+      };
+
+      // Add to exp array
+      profile.education.unshift(newEdu);
+
+      profile.save().then((profile) => res.json(profile));
+    });
+  }
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    let errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        // Get remove index
+        const removeIndex = profile.experience
+          .map((item) => item.id)
+          .indexOf(req.params.exp_id);
+
+        if (removeIndex === -1) {
+          errors.experiencenotfound = "Experience not found";
+          // Return any errors with 404 status
+          return res.status(404).json(errors);
+        }
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then((profile) => res.json(profile));
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map((item) => item.id)
+          .indexOf(req.params.edu_id);
+
+        if (removeIndex === -1) {
+          errors.educationnotfound = "Education not found";
+          // Return any errors with 404 status
+          return res.status(404).json(errors);
+        }
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then((profile) => res.json(profile));
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
 
 module.exports = router;
